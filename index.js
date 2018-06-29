22a80187d8a9e4d2eda787a7cc8908c1974a19a1
Sean Oughton <seanoughton@gmail.com>
2018-06-29 23:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+//that pulls a .target out of #nested (# is used for IDs in selectors — but you knew that because you read the docs, right? :) ). (Note that in index.html #nested and .target just happen to be divs. This method should work with arbitrary elements.)
+
+function nestedTarget() {
+
+};
